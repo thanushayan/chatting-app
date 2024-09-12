1cd379f984e0ac1de46d6fac90a24aeb119f1a03
thanushayan <thanushayan17@gmail.com>
2024-09-12 09:33:07 +0530

    message model

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -81,7 +81,7 @@ export const login =async (req, res) => {
      
 };
 
-export const logout =  async (req, res) => {
+export const logout =   (req, res) => {
     try{
         res.cookie("jwt","",{maxAge:0})
         res.status(200).json({message:"Logged out successfully"})
