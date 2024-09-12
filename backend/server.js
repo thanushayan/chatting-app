7eb0c1e4bde9b51d837dc2cc079831741b483892
thanushayan <thanushayan17@gmail.com>
2024-09-12 09:02:15 +0530

    backend wwork

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+dotenv.config();
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    // Root route http://localhost:5000/
+    res.send("Hello world!!");
+});
+
+// Use the auth routes
+app.use("/api/auth", authRoutes);
+
+// Connect to MongoDB and start the server
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`);
+});
