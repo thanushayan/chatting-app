dbc18c1035a77430f72297e7d2abea4b9b474ebb
thanushayan <thanushayan17@gmail.com>
2024-09-13 18:41:14 +0530

    home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const Home = () => {
-  return <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding
-  backdrop-filter backdrop-blur-lg bg-opacity-0'>Home</div>
-    
-   
-}
+import MessageContainer from "../../components/messages/MessageContainer";
+import Sidebar from "../../components/sidebar/Sidebar";
 
-export default Home
+const Home = () => {
+	return (
+		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<Sidebar />
+			<MessageContainer />
+		</div>
+	);
+};
+export default Home;
